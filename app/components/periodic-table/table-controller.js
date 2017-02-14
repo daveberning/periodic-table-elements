c07020ba709dcc18613d86afdd072566deec1908
Dave Berning <daveberning@icloud.com>
2017-02-13 23:12:41 -0500

    JSON elements, symbols, atom weight

--- app/components/periodic-table/table-controller.js
+++ app/components/periodic-table/table-controller.js
@@ -1,23 +1,40 @@
 var periodicTable = angular.module("periodicTableOfElements");
 
-periodicTable.controller("subjectController", function($scope, elementsFactory) {
-    $scope.subject = "science";
-});
-
-periodicTable.controller('elementNames', function($scope, elementsFactory){
+periodicTable.controller('elementNamesController', function($scope, elementsFactory){
     elementsFactory.getItems().then(function(response){
         $scope.items = response.data;
         var tableLength = $scope.items.table.length;
+        var actinoidsLength = $scope.items.actinoids.length;
+        var lanthanoidsLength = $scope.items.lanthanoids.length;
+        var elementNames = [];
+
+        $scope.itemsPerPage = 5;
+        $scope.currentPage = 0;
+        $scope.elementNames = elementNames;
         console.log($scope.items.table);
 
         for (var i = 0; i < tableLength; i++) { // get elements in tables
             var elementLength = $scope.items.table[i].elements.length;
 
             for (a = 0; a < elementLength; a++) { // get element names
-                var elementName = $scope.items.table[i].elements[a].name;
+                var elementName = $scope.items.table[i].elements[a];
                 $scope.elementName = elementName;
-                console.log(elementName);
+                elementNames.push(elementName);
             }
         }
+
+        for (var i = 0; i < actinoidsLength; i++) { // get elements in tables
+            var elementName = $scope.items.actinoids[i];
+            $scope.elementName = elementName;
+            elementNames.push(elementName);
+        }
+
+        for (var i = 0; i < lanthanoidsLength; i++) { // get elements in tables
+            var elementName = $scope.items.lanthanoids[i];
+            $scope.elementName = elementName;
+            elementNames.push(elementName);
+        }
+
+        console.log(elementNames);
     });
 });
